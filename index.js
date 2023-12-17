a12a9a5cb0a3457978034f21310b72fbea67beb6
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2023-12-17 15:34:16 +0530

    Socket

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const  {createServer} = require('node:http')
+const {Server} = require('socket.io')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
@@ -6,14 +8,33 @@ const DB = require('./DB')
 const port = process.env.PORT
 
 const app = express()
+
+
 dotenv.config()
+
 app.use(cors())
+
+const server = createServer(app)
+const io =new Server(server,{cors:{
+  origins:'https://localhost:5173'
+}})
 app.use(express.json())
 app.use('/auth', authRouter)
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send('Something broke!')
 })
-app.listen(port, () => {
+
+io.on('connection',(socket)=>{
+  console.log('connected on socket')
+  socket.on('offer',(offer)=>{
+    
+    console.log("got something",offer)
+  })
+  socket.on('disconnect',()=>{
+    console.log("diconnected")
+  })
+})
+server.listen(port, () => {
   console.log(`app listening on ${port}`)
 })
